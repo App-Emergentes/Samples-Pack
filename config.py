8d50a4d6d04befe9ba99dd0c557036b1c5b2fb86
App-Emergentes <zmeebabys@gmail.com>
2025-04-13 16:37:41 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = 12345678  # Reemplazar con tu API_ID de my.telegram.org
+API_HASH = "tu_api_hash"  # Reemplazar con tu API_HASH
+BOT_TOKEN = "tu_bot_token"  # Token del bot
+CANAL_USERNAME = "@tu_canal"  # Username del canal
+CANAL_URL = "https://t.me/tu_canal"  # Link al canal para mostrar
